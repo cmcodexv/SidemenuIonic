678847b2ffc602743ce7e808a27c53b3890676f4
Carlos Maradiaga <[cmaradiaga715@gmail.com]>
2021-05-21 15:58:16 -0600

    problemas al instanciar header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CarrersComponent } from './components/carrers/carrers.component';
+import { HomeComponent } from './components/home/home.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: ':id',
+//     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+//   }
+// ];
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: ':id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  { path: 'home', component: HomeComponent },
+  { path: 'carrers', component: CarrersComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 @NgModule({
--- src/app/components/carrers/carrers.component.spec.ts
+++ src/app/components/carrers/carrers.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-  let component: FolderPage;
-  let fixture: ComponentFixture<FolderPage>;
+import { CarrersComponent } from './carrers.component';
+
+describe('CarrersComponent', () => {
+  let component: CarrersComponent;
+  let fixture: ComponentFixture<CarrersComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FolderPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ CarrersComponent ],
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FolderPage);
+    fixture = TestBed.createComponent(CarrersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
